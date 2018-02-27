542dedb4cb77ec99609d0eb85414ef7784e4562a
leonfrisch <leon.frisch37@gmail.com>
2018-02-27 21:52:19 +0100

    Renamed since it is a controller.

--- src/Controller/ManualAddController.java
+++ src/Controller/ManualAddController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManualAdd {
+public class ManualAddController {
 
     @FXML public Label labelNameSeries;
     @FXML public TextField nameSeries;
