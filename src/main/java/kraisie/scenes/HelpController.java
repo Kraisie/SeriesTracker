5444d8e062d07d08080a32e662d67b9840b2ad8d
Kraisie <frolen307@gmail.com>
2021-10-06 09:18:19 +0200

    Load help screen from web instead of parsing from local file

--- src/main/java/kraisie/scenes/HelpController.java
+++ src/main/java/kraisie/scenes/HelpController.java
@@ -5,17 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import kraisie.dialog.BrowserControl;
-import kraisie.ui.MdParser;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class HelpController {
 
@@ -24,32 +18,10 @@ public class HelpController {
 
 	@FXML
 	private void initialize() {
-		String content = readHelp();
 		webView.setContextMenuEnabled(false);
 		WebEngine webEngine = webView.getEngine();
-		webEngine.loadContent(content);
+		webEngine.load("https://github.com/Kraisie/SeriesTracker/blob/master/README.md");
 		addHyperLinkListeners(webEngine);
-	}
-
-	private String readHelp() {
-		Path path = Paths.get(System.getProperty("user.home"), "/SERIESTRACKER/README.html");
-		if (path.toFile().exists()) {
-			return getFileContent(path);
-		}
-
-		MdParser parser = new MdParser("README.md");
-		return parser.toHtml();
-	}
-
-	private String getFileContent(Path path) {
-		try {
-			return Files.readString(path);
-		} catch (IOException e) {
-			e.printStackTrace();
-			// TODO: log error can't read file
-		}
-
-		return "<h1>Error reading file content!</h1>";
 	}
 
 	private void addHyperLinkListeners(WebEngine webEngine) {
